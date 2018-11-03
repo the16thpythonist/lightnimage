5819ba39faead0ba42d55bdf690bb3e342a4f72e
the16thpythonist <jonseb1998@gmail.com>
2018-11-03 16:22:11 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+
+setup(
+    name='lightnimage',
+    version='0.1',
+    description='A library to analyze images of lightning strikes',
+    url='',
+    author='Jonas Teufel',
+    author_email='jonseb1998@gmail.com',
+    license='MIT',
+    packages=[
+        'lightnimage'
+    ],
+    zip_safe=False
+)
