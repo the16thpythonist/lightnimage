ac4cfee6f1432e6faf8e5351c089889ff85b82b6
the16thpythonist <jonseb1998@gmail.com>
2018-12-05 16:52:52 +0100

    new version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='lightnimage',
-    version='0.0.0.10',
+    version='0.0.0.11',
     description='A library to analyze images of lightning strikes',
     url='https://github.com/the16thpythonist/lightnimage',
     author='Jonas Teufel',
@@ -17,7 +17,8 @@ setup(
         'scipy',
         'matplotlib',
         'jupyter',
-        'pillow'
+        'pillow',
+        'imageio'
     ],
     python_requires='~=3.5',
     zip_safe=False
